6070361ae7c02ff09b23fc92c68c0dc40d9d9a1b
Victor Ridao Chaves <victormanuel.ridaochaves@adaits.es>
2025-10-10 21:19:13 +0200

    Ejercicios básicos.

---
+++ t1/ej5/ejercicios/basico.js
@@ -0,0 +1,36 @@
+// Función que reciba un objeto { bien: N, mal, M } y devuelva el resultado de la resta
+
+/*function resultado(coso) {
+    return coso.bien - coso.mal;
+}
+
+const resultado = {bien: 10, mal: 7};
+
+console.log(resultado(resultado));*/
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// Función que reciba las dimensiones de un cubo en un objeto y devuelva el volumen { ancho: 2, largo: 5, alto: 1 }
+
+/*function volumen(cubo) {
+    return cubo.ancho * cubo.largo * cubo.alto;
+}
+
+const cubo = {ancho: 2, largo: 5, alto: 1};
+
+console.log(volumen(cubo));*/
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// Recoger el resto de un objeto en la variable ‘rest’. Sólo hay que editar el código
+
+const yo = {
+    nombre1: 'Victor',
+    nombre2: 'Manuel',
+    apellido1: 'Ridao',
+    apellido2: 'Chaves',
+    edad: 27
+};
+
+const {...rest} = yo;
+console.log(rest);
