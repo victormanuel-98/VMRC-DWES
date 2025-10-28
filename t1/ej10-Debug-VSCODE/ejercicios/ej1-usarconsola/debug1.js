58deb014c1ab537f5059e3e1348a014aa069ec27
Victor Ridao Chaves <victormanuel.ridaochaves@adaits.es>
2025-10-28 16:58:09 +0100

    Se crea la carpeta de ejercicios y se añaden las carpetas de los seis primeros, se finaliza el ejercicio 1.

---
+++ t1/ej10-Debug-VSCODE/ejercicios/ej1-usarconsola/debug1.js
@@ -0,0 +1,13 @@
+// 1. Usar la consola en medio de una ejecucíonn para depurar el código
+
+function suma(a, b) {
+    const resultado = a + b;
+    console.log("Calculando suma...");
+    return resultado;
+}
+
+const x = 5;
+const y = 10;
+const total = suma(x, y);
+
+console.log("El resultado es:", total);
