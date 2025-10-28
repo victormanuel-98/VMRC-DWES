24da39422c1f4310793c826faa222db9c8dcd8e1
Victor Ridao Chaves <victormanuel.ridaochaves@adaits.es>
2025-10-28 17:59:40 +0100

    Se añade el enunciado del trabajo.

---
+++ t1/ej10-Debug-VSCODE/ejercicios/trabajoextra/index.js
@@ -0,0 +1,5 @@
+/*
+Crear configuraciones de depuración en vscode para el proyecto de notas.
+Recomendación: Configurar en los ajustes de usuario para ejecutar el fichero actual y en el
+launch.json del proyecto para ejecutar el index.js.
+*/
